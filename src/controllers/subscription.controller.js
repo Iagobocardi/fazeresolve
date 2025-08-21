fffd588add9539e6055a6b674bc3b1e75aefe25a
iagynn <iagoboss346@gmail.com>
2025-08-20 23:48:06 -0300

    Update subscription.controller.js

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -27,11 +27,7 @@ const handleCreatePlan = async (req, res) => {
  */
 const handleSubscribe = async (req, res) => {
     try {
-        console.log("--- [DEBUG] Cabeçalhos recebidos no backend ---");
-        console.log(JSON.stringify(req.headers, null, 2));
-
-        const { cardTokenId } = req.body;
-        const deviceId = req.header('X-meli-session-id');
+        const { cardTokenId, deviceId } = req.body;
         const userId = req.user.id;
 
         if (!cardTokenId) {
