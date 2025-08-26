2dd9226e2ee1ba10dca479f32c7773f76f7a7f31
iagynn <iagoboss346@gmail.com>
2025-08-26 11:32:07 -0300

    Update agendamentos.routes.js

--- src/routes/agendamentos.routes.js
+++ src/routes/agendamentos.routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const adminController = require('../controllers/admin.controller');
 const agendamentosController = require('../controllers/agendamentos.controller');
 const { agendamentoValidationRules } = require('../controllers/agendamentos.controller');
 const { validate } = require('../middlewares/validation.middleware');
 const authMiddleware = require('../middlewares/auth.middleware');
 const roleMiddleware = require('../middlewares/role.middleware');
+
+// Rota de login para administradores (não protegida por autenticação)
+router.post('/login', adminController.loginAdmin);
+
 
 // Aplica o middleware de autenticação e verificação de função a todas as rotas
 router.use(authMiddleware);
