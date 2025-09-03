1ba4bc65f379a21bd8ab9dd2a1f0a833edf53cd7
iagynn <iagoboss346@gmail.com>
2025-09-03 10:06:10 -0300

    Update fornecedores.routes.js

--- src/routes/fornecedores.routes.js
+++ src/routes/fornecedores.routes.js
@@ -4,17 +4,16 @@ const express = require('express');
 const router = express.Router();
 const fornecedorController = require('../controllers/fornecedor.controller.js');
 const authMiddleware = require('../middlewares/auth.middleware');
-const roleMiddleware = require('../middlewares/role.middleware');
+const checkPermission = require('../middlewares/checkPermission.middleware');
 
-// Aplica o middleware de autenticação e verificação de função a todas as rotas
+// Aplica o middleware de autenticação a todas as rotas
 router.use(authMiddleware);
-router.use(roleMiddleware(['Dono', 'ADMIN']));
 
 // Rota para listar todos os fornecedores
 // GET /api/fornecedores
-router.get('/', fornecedorController.listarFornecedores);
+router.get('/', checkPermission('ver_clientes'), fornecedorController.listarFornecedores);
 
 // POST /api/fornecedores
-router.post('/', fornecedorController.criarFornecedor);
+router.post('/', checkPermission('editar_clientes'), fornecedorController.criarFornecedor);
 
 module.exports = router;
