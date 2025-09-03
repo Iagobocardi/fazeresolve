0ba661ae899d73ed8ff68e15a48269952bc787d7
iagynn <iagoboss346@gmail.com>
2025-09-03 10:06:47 -0300

    Update agendamentos.routes.js

--- src/routes/agendamentos.routes.js
+++ src/routes/agendamentos.routes.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const adminController = require('../controllers/admin.controller');
 const agendamentosController = require('../controllers/agendamentos.controller');
 const { agendamentoValidationRules } = require('../controllers/agendamentos.controller');
 const { validate } = require('../middlewares/validation.middleware');
 const authMiddleware = require('../middlewares/auth.middleware');
-const roleMiddleware = require('../middlewares/role.middleware');
+const checkPermission = require('../middlewares/checkPermission.middleware');
 
-// Rota de login para administradores (não protegida por autenticação)
-router.post('/login', adminController.loginAdmin);
-
-
-// Aplica o middleware de autenticação e verificação de função a todas as rotas
+// Aplica o middleware de autenticação a todas as rotas
 router.use(authMiddleware);
-router.use(roleMiddleware(['Dono', 'ADMIN']));
 
-router.get('/', agendamentosController.getAllAgendamentos);
-router.get('/:id', agendamentosController.getAgendamentoById);
-router.post('/', agendamentoValidationRules, validate, agendamentosController.createAgendamento);
-router.put('/:id', agendamentoValidationRules, validate, agendamentosController.updateAgendamento);
-router.delete('/:id', agendamentosController.deleteAgendamento);
+// As verificações de permissão são aplicadas individualmente a cada rota
+router.get('/', checkPermission('ver_agenda'), agendamentosController.getAllAgendamentos);
+router.get('/:id', checkPermission('ver_agenda'), agendamentosController.getAgendamentoById);
+router.post('/', checkPermission('editar_agenda'), agendamentoValidationRules, validate, agendamentosController.createAgendamento);
+router.put('/:id', checkPermission('editar_agenda'), agendamentoValidationRules, validate, agendamentosController.updateAgendamento);
+router.delete('/:id', checkPermission('editar_agenda'), agendamentosController.deleteAgendamento);
 
 module.exports = router;
