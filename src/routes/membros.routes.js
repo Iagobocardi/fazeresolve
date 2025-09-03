f3656b34c5cc5338623d0d8b0a96a2d2652a1a04
iagynn <iagoboss346@gmail.com>
2025-09-03 09:45:22 -0300

    Update membros.routes.js

--- src/routes/membros.routes.js
+++ src/routes/membros.routes.js
@@ -8,7 +8,7 @@ const roleMiddleware = require('../middlewares/role.middleware');
 
 // Aplica o middleware de autenticação e verificação de função a todas as rotas
 router.use(authMiddleware);
-router.use(roleMiddleware(['PRESTADOR', 'ADMIN']));
+router.use(roleMiddleware(['Dono', 'ADMIN']));
 
 // Protege a rota de criação, primeiro verificando se a assinatura está ativa, depois se o limite de usuários foi atingido.
 router.post('/', checkSubscription, checkUserLimit, membroController.criarMembro);
