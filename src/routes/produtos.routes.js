90f0723ca431fce595f06e322320138dc124bf85
iagynn <iagoboss346@gmail.com>
2025-09-03 09:45:44 -0300

    Update produtos.routes.js

--- src/routes/produtos.routes.js
+++ src/routes/produtos.routes.js
@@ -8,7 +8,7 @@ const roleMiddleware = require('../middlewares/role.middleware');
 
 // Aplica o middleware de autenticação e verificação de função a todas as rotas
 router.use(authMiddleware);
-router.use(roleMiddleware(['PRESTADOR', 'ADMIN']));
+router.use(roleMiddleware(['Dono', 'ADMIN']));
 
 // Rotas para o CRUD de produtos
 router.post('/', produtosController.createProduto);
