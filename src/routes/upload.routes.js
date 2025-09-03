660d79b8fc407de8d297bfcd83382aa76123412d
iagynn <iagoboss346@gmail.com>
2025-09-03 09:46:41 -0300

    Update upload.routes.js

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -15,7 +15,7 @@ const roleMiddleware = require('../middlewares/role.middleware');
 
 // Aplica o middleware de autenticação e verificação de função a todas as rotas
 router.use(authMiddleware);
-router.use(roleMiddleware(['PRESTADOR', 'ADMIN']));
+router.use(roleMiddleware(['Dono', 'ADMIN']));
 
 // POST /api/upload/product-image
 router.post(
