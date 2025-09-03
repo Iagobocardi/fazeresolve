a266d99de3203eb2ed7336f5836198fc10bdf3c0
iagynn <iagoboss346@gmail.com>
2025-09-03 09:45:02 -0300

    Update fornecedores.routes.js

--- src/routes/fornecedores.routes.js
+++ src/routes/fornecedores.routes.js
@@ -8,7 +8,7 @@ const roleMiddleware = require('../middlewares/role.middleware');
 
 // Aplica o middleware de autenticação e verificação de função a todas as rotas
 router.use(authMiddleware);
-router.use(roleMiddleware(['PRESTADOR', 'ADMIN']));
+router.use(roleMiddleware(['Dono', 'ADMIN']));
 
 // Rota para listar todos os fornecedores
 // GET /api/fornecedores
