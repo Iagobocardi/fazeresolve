f3396ecad0aa9b63339716bda2211d4bf37b337d
iagynn <iagoboss346@gmail.com>
2025-09-04 21:05:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -84,8 +84,7 @@ const corsOptions = {
         }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'cache-control', 'expires'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
 };
 
 app.use(cors(corsOptions));
