c9d95cef57a2366fa1de98b1d6cc8007f1d014ab
iagynn <iagoboss346@gmail.com>
2025-09-06 18:52:28 -0300

    Create transacao.model.js

---
+++ src/models/transacao.model.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const transacaoSchema = new mongoose.Schema({
+    contaId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Conta',
+        required: true,
+        index: true
+    },
+    tipo: {
+        type: String,
+        enum: ['Receita', 'Despesa'],
+        required: true,
+        index: true
+    },
+    descricao: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    valor: {
+        type: Number,
+        required: true
+    },
+    categoria: {
+        type: String,
+        trim: true,
+        default: 'Outros'
+    },
+    data: {
+        type: Date,
+        default: Date.now,
+        index: true
+    },
+    orcamentoAssociado: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Orcamento'
+    },
+    metodoPagamento: {
+        type: String,
+        trim: true
+    }
+}, {
+    timestamps: true
+});
+
+// Para garantir que o valor seja sempre positivo, embora o tipo ('Receita'/'Despesa') o diferencie.
+transacaoSchema.pre('save', function(next) {
+    if (this.valor < 0) {
+        this.valor = this.valor * -1;
+    }
+    next();
+});
+
+const Transacao = mongoose.model('Transacao', transacaoSchema);
+
+module.exports = Transacao;
