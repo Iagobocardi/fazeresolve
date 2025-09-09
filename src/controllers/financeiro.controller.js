69c6d1d11aee806ff99e0017586f48950d41aa76
iagynn <iagoboss346@gmail.com>
2025-09-09 19:10:56 -0300

    Update financeiro.controller.js

--- src/controllers/financeiro.controller.js
+++ src/controllers/financeiro.controller.js
@@ -130,9 +130,6 @@ const createManualTransacao = async (req, res) => {
             novaTransacao.comprovanteUrl = `/uploads/${req.file.filename}`;
         }
 
-        // HACK: Lançar um erro para depurar o objeto antes de salvar
-        throw new Error(`DEBUG: ${JSON.stringify(novaTransacao)}`);
-
         const transacaoSalva = await novaTransacao.save();
         res.status(201).json(transacaoSalva);
 
