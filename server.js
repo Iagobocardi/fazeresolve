048f5de406d02bc751336baa3e8f90fe65848b7a
iagynn <iagoboss346@gmail.com>
2025-09-10 17:06:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -94,10 +94,9 @@ const corsOptions = {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma', 'Expires'],
+    preflightContinue: true,
 };
 
-// Habilita o pre-flight para todas as rotas, garantindo que as requisições OPTIONS sejam tratadas primeiro.
-app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
