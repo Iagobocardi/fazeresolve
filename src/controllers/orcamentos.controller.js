6b20cdd7d6393c9cfd80fbe9b05e2f29a65647e5
iagynn <iagoboss346@gmail.com>
2025-09-10 22:27:18 -0300

    Update orcamentos.controller.js

--- src/controllers/orcamentos.controller.js
+++ src/controllers/orcamentos.controller.js
@@ -301,7 +301,7 @@ const createOrcamento = async (req, res) => {
             await whatsappService.sendWhatsAppMessage(conta.telefone, notificationToPrestador);
         }
 
-        res.status(201).json({ orcamentoSalvo, debug_cliente: cliente });
+        res.status(201).json(orcamentoSalvo);
     } catch (error) {
         console.error("Erro detalhado ao criar orçamento:", error);
         res.status(500).json({ 
