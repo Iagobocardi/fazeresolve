51c65b508f8fb7d84d9dae4ac25ff7e517634360
iagynn <iagoboss346@gmail.com>
2025-09-15 14:15:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ const subscriptionRoutes = require('./src/routes/subscription.routes.js');
 const uploadRoutes = require('./src/routes/upload.routes.js');
 const whatsappRoutes = require('./src/routes/whatsapp.routes.js');
 const whatsappTemplateRoutes = require('./src/routes/whatsappTemplates.routes.js');
-const utilsRoutes = require('./src/routes/utils.routes.js');
 
 // Importação do Middleware
 const errorMiddleware = require('./src/middlewares/error.middleware');
@@ -139,7 +138,6 @@ app.use('/api/focusnfe', providerAuthMiddlewares, focusnfeRoutes);
 app.use('/api/permissoes', providerAuthMiddlewares, permissoesRoutes);
 app.use('/api/invoices', providerAuthMiddlewares, invoiceRoutes);
 app.use('/api/membros', providerAuthMiddlewares, membroRoutes);
-app.use('/api/utils', providerAuthMiddlewares, utilsRoutes);
 
 
 // Rota de teste
