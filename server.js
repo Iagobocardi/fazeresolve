f2a603808fa1c8ef359c7409c3f13dbfe5240ab0
iagynn <iagoboss346@gmail.com>
2025-09-15 16:51:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
