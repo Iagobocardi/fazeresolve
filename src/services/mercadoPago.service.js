e8aa3cfa1bca49129e758a9838e758e7a3147c2e
iagynn <iagoboss346@gmail.com>
2025-09-16 11:26:31 -0300

    Update mercadoPago.service.js

--- src/services/mercadoPago.service.js
+++ src/services/mercadoPago.service.js
@@ -59,4 +59,11 @@ const handlePaymentNotification = async (paymentId) => {
 
 module.exports = {
     handlePaymentNotification,
+    createPixPayment: async (paymentData) => {
+        const client = new MercadoPagoConfig({ accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN });
+        const payment = new Payment(client);
+
+        const result = await payment.create({ body: paymentData });
+        return result;
+    }
 };
