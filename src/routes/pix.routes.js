7640408cfe8e4df0209a7d62fbfccc2cb19e5b83
iagynn <iagoboss346@gmail.com>
2025-09-16 16:15:30 -0300

    Update pix.routes.js

--- src/routes/pix.routes.js
+++ src/routes/pix.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const pixController = require('../controllers/pix.controller.js');
-const authMiddleware = require('../middlewares/auth.middleware.js');
+const provisionalAuthMiddleware = require('../middlewares/provisionalAuth.middleware.js');
 
 router.post(
     '/charge',
-    authMiddleware,
+    provisionalAuthMiddleware,
     pixController.createPixCharge
 );
 
