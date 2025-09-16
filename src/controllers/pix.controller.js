72c6413adce4d38345417bb7a80d73234c553ff1
iagynn <iagoboss346@gmail.com>
2025-09-16 13:27:33 -0300

    Update pix.controller.js

--- src/controllers/pix.controller.js
+++ src/controllers/pix.controller.js
@@ -1,15 +1,3 @@
-const subscriptionService = require('../services/subscription.service.js');
-
-exports.createPixCharge = async (req, res) => {
-    try {
-        res.status(200).json({ message: 'Pix charge created successfully.' });
-    } catch (error) {
-        console.error("Erro ao criar cobrança Pix:", error);
-        res.status(500).json({ message: 'Erro interno ao criar cobrança Pix.' });
-    }
-};
-const subscriptionService = require('../services/subscription.service.js');
-
 const mercadoPagoService = require('../services/mercadoPago.service.js');
 
 exports.createPixCharge = async (req, res) => {
