f9c99d5be837fd55b7c00342a1fb607e9e98f240
iagynn <iagoboss346@gmail.com>
2025-09-24 14:02:57 -0300

    Update catalogo.routes.js

--- src/routes/catalogo.routes.js
+++ src/routes/catalogo.routes.js
@@ -8,28 +8,31 @@ const {
     updateItemPessoal,
     deleteItemPessoal
 } = require('../controllers/catalogo.controller');
+const checkPermission = require('../middlewares/checkPermission.middleware');
 
 // --- Rotas do Catálogo de Mercado (Faz&Resolve) ---
 
 // GET /api/catalogo/mercado?area=:areaDeAtuacao
-router.get('/mercado', getItensMercado);
+router.get('/mercado', checkPermission('usar_catalogo_inteligente'), getItensMercado);
 
 // GET /api/catalogo/mercado/precos-regionais
 router.get('/mercado/precos-regionais', getPrecosRegionais);
 
 
+const hasPermission = checkPermission('usar_catalogo_inteligente');
+
 // --- Rotas do Catálogo Pessoal (do Usuário) ---
 
 // GET /api/catalogo/pessoal
-router.get('/pessoal', getItensPessoais);
+router.get('/pessoal', hasPermission, getItensPessoais);
 
 // POST /api/catalogo/pessoal
-router.post('/pessoal', createItemPessoal);
+router.post('/pessoal', hasPermission, createItemPessoal);
 
 // PUT /api/catalogo/pessoal/:id
-router.put('/pessoal/:id', updateItemPessoal);
+router.put('/pessoal/:id', hasPermission, updateItemPessoal);
 
 // DELETE /api/catalogo/pessoal/:id
-router.delete('/pessoal/:id', deleteItemPessoal);
+router.delete('/pessoal/:id', hasPermission, deleteItemPessoal);
 
 module.exports = router;
