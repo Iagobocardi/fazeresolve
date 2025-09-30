e4ddcf7f61c0e01750e074568f985d28b4534831
iagynn <196245823+iagynn@users.noreply.github.com>
2025-09-30 14:39:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -64,6 +64,7 @@ const cors = require('cors');
 // Lista de origens permitidas, agora usando Regex para mais flexibilidade
 const allowedOrigins = [
     'https://app.fazeresolve.com',
+    'https://fazeresolve.onrender.com',
     /^https:\/\/(www\.)?fazeresolve\.onrender\.com$/, // Permite com e sem 'www'
     /^http:\/\/localhost(:\d+)?$/,                 // Permite qualquer porta em localhost
     'https://accounts.google.com',
