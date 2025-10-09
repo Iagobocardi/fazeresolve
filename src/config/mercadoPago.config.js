0e71b5b2e40bde436fe5532533696b5c9490007a
iagynn <196245823+iagynn@users.noreply.github.com>
2025-10-09 11:28:27 -0300

    Update mercadoPago.config.js

--- src/config/mercadoPago.config.js
+++ src/config/mercadoPago.config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const mercadoPagoConfig = {
     accessToken: process.env.MP_ACCESS_TOKEN,
+    appId: process.env.MP_APP_ID, // Adicionado o APP_ID para o fluxo OAuth
 };
 
 module.exports = mercadoPagoConfig;
