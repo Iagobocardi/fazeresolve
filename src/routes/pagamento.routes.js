8ebcffbbe9374708a4ea35a4b81cbd878cb18129
iagynn <196245823+iagynn@users.noreply.github.com>
2025-10-21 11:09:11 -0300

    Update pagamento.routes.js

--- src/routes/pagamento.routes.js
+++ src/routes/pagamento.routes.js
@@ -4,7 +4,7 @@ const { createOneTimePayment, handleWebhook } = require('../controllers/pagament
 const authMiddleware = require('../middlewares/auth.middleware.js');
 
 // Rota para criar um pagamento único (PIX ou Cartão)
-router.post('/criar-pagamento-unico', authMiddleware, createOneTimePayment);
+router.post('/onetime', authMiddleware, createOneTimePayment);
 
 // Rota pública para receber webhooks de notificação do Mercado Pago
 router.post('/mercado-pago-webhook', handleWebhook);
