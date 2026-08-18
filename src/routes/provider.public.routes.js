03d721c169665b8be8937ee1cc74afaf68bdde5d
iago <iagoboss346@gmail.com>
2026-08-18 12:09:40 -0300

    refactor: remove legacy root files

--- src/routes/provider.public.routes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/provider.public.routes.js
-const express = require('express');
-const router = express.Router();
-const providerController = require('../controllers/provider.controller.js');
-
-// Rota PÚBLICA para o callback do Mercado Pago OAuth
-router.get('/mercadopago-callback', providerController.handleMercadoPagoCallback);
-
-module.exports = router;
